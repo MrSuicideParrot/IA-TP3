a9090235a8c0da1aa4beb443e9fcbb3448aa01eb
André Cirne <ancirne@gmail.com>
2017-04-02 18:20:34 +0100

    Regressão

--- rota.py
+++ rota.py
@@ -1,3 +1,5 @@
+
+
 class Carreiras:
     def __init__(self,data_Inicio, data_Fim, numeroID, dias):
         self.data_Inicio = data_Inicio
@@ -13,41 +15,5 @@ class Percursos:
         self.partida = partida
         self.fim = fim
         self.viagens = []
-
-
-# incio do script em si
-def create_percurso(n):
-    n = n.split('(')
-    print(n)
-    print(len(n))
-    n = n[1]
-    n = n.split(',',maxsplit=2)
-    tempos[n[0]] = Percursos(n[0],n[1])
-    rotas = n[2]
-    rotas = rotas.replace(')', '')
-    rotas = rotas[1:-2].split('/')
-    while not rotas:
-        print(rotas)
-
-        #casos diferentes
-        if x[0] == '[':
-            print(x)
-            x = x[1:]
-        if x[-1] == '[':
-            x = x[-1]
-
-        #lista tratamento normal
-        x = x.split(',')
-        print(x)
-     #   tempos[n[0]].viagens.append(Carreiras(*x))
-
-
-
-tempos = {}
-
-with open('times.pl','r') as fd:
-    buffer = fd.read().replace('\n','').split('.')
-    for n in buffer:
-        create_percurso(n)
 
 
