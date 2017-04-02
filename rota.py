fe463dcf55ad4da22f3aba6f736af6d8a6165501
André Cirne <ancirne@gmail.com>
2017-04-02 23:05:01 +0100

    Parsing do ficheiro
    Quase tudo feito so falta acertar com a ultima parte do ficheiro

--- rota.py
+++ rota.py
@@ -1,33 +1,52 @@
-from pyparsing import Word, alphas, Literal
+from pyparsing import Word, alphas, Literal, Suppress, Optional, nums, alphanums, OneOrMore
+
 
 class Carreiras:
     def __init__(self,data_Inicio, data_Fim, numeroID, dias):
-        self.data_Inicio = data_Inicio
-        self.data_Fim = data_Fim
+        diasPars = Suppress('[') + OneOrMore(Word(alphas) + Optional(Suppress(','))) + Suppress(']')
+        self.data_Inicio = Tempo(data_Inicio)
+        self.data_Fim = Tempo(data_Fim)
         self.numeroID = numeroID
         if dias == 'alldays':
-            self.dias = ['mo','tu','we', 'th', 'fr', 'sa','su']
+            self.dias = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su']
         else:
-            pass
+            self.dias = diasPars.parseString(dias)
+
 
 class Percursos:
-    def __init__(self,partida,fim):
+    def __init__(self, partida, fim):
         self.partida = partida
         self.fim = fim
         self.viagens = []
 
+
 class Tempo:
-    def __init__(self,horas,minutos):
-        self.horas
-        self.minutos
+    def __init__(self, tempo):
+        tempoPars = Word(nums) + Suppress(':') + Word(nums)
+        tempo = tempoPars.parseString(tempo)
+        self.horas = int(tempo[0])
+        self.minutos = int(tempo[1])
+
+tempo = []
 
 ''' READING FILE'''
-fd = open('times.pl','r')
-buffer=fd.read()
-buffer.split('\n')
-
+fd = open('times.pl', 'r')
+buffer=fd.read().split('\n')
 '''Parsing lines'''
 
 '''Diferentes tipos de parsers'''
-firstLine = Literal('timetable') + '(' + Word(alphas) +',' + Word(alphas) + ',\n'
-for line in buffer:
+firstLine = Literal('timetable') + Suppress('(') + Word(alphas) +Suppress(',') + Word(alphas) + Suppress(',')
+carreiraLine = Optional(Suppress('[ ')) + Word(nums+':'+nums) + Suppress('/') + Word(nums+':'+nums) + Suppress('/') + Word(alphanums) + Suppress('/') + Literal('alldays') ^ ('['+OneOrMore(Word(alphas)+Optional(','))+']') + (Suppress(']).') ^ Suppress(','))
+i = 0
+index_tempo = 0
+while not buffer == ['']:
+    aux = firstLine.parseString(buffer[0])
+    tempo.append(Percursos(aux[1], aux[2]))
+    del buffer[0]
+    while not buffer[0] == '':
+        print(buffer)
+        aux = carreiraLine.parseString(buffer[0])
+        tempo[index_tempo].viagens.append(Carreiras(*aux))
+        del buffer[0]
+
+    ++index_tempo
