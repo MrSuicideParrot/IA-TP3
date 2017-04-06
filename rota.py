6320d958663b362a934002c282e4b760080c68de
André Cirne <ancirne@gmail.com>
2017-04-06 11:13:09 +0100

    A vida loca de IA continua,em teoria entramos na faze final do programa

--- rota.py
+++ rota.py
@@ -12,6 +12,9 @@ class Carreiras:
         else:
             self.dias = diasPars.parseString(dias)
 
+    def __str__(self):
+        return self.numeroID+"/"+self.data_Inicio+"/"+self.data_Fim
+
 
 class Percursos:
     def __init__(self, partida, fim):
@@ -27,14 +30,68 @@ class Tempo:
         self.horas = int(tempo[0])
         self.minutos = int(tempo[1])
 
+    def transfer(self,t2):
+        t1 = self
+        return (t2.horas - t1.horas)*60 + (t2.minutos - t1.minutos) <= 40
 
 class Arvore:
     def __init__(self, nos):
-        self.nodes = nos
-        self.edges = [[False for _ in range(len(nos))] for _ in range(len(nos))]
-        for j in range(len(nos)):
-            self.edges[j][findIndex(nos[j].fim, nos)] = True
 
+        '''Criação de dicionario e organização'''
+        self.dicionario = {}
+        posic = 0
+        for aux in nos:
+            if aux.partida not in self.dicionario:
+                self.dicionario[aux.partida] = posic
+                posic += 1
+            if aux.fim not in self.dicionario:
+                self.dicionario[aux.fim] = posic
+                posic += 1
+
+        self.edges = [[None for _ in range(len(self.dicionario))] for _ in range(len(self.dicionario))]
+        self.edgesVeracidade = [[False for _ in range(len(self.dicionario))] for _ in range(len(self.dicionario))] #usra como o valor de True como ainda nao visitado
+
+        for aux in nos:
+            posicINI = self.dicionario[aux.partida]
+            posicFIM = self.dicionario[aux.fim]
+            self.edges[posicINI][posicFIM] = aux.viagens
+            self.edgesVeracidade[posicINI][posicFIM] = True
+
+
+
+
+    def search(self, partida, destino, day):
+        startIndex = self.dicionario[partida]
+        endIndex = self.dicionario[destino]
+
+        '''Caso exista um voo direto'''
+        resposta = None
+        for aux in self.edges[startIndex][endIndex]:
+            if day in aux.dias:
+                resposta = aux.__str__()
+
+        if resposta is not None:
+            print(partida+"/"+destino+"/"+resposta)
+            return
+
+        '''Voo indireto usando dfs'''
+        resposta = self.dfs(startIndex, endIndex, day)
+
+
+    def dfs(self, partida, destino, hchegada, day, rota = []):
+        '''Caso de paragem'''
+        if partida == destino:
+            return []
+
+        resAux = None
+        for i in range(len(self.dicionario)):
+            if self.edgesVeracidade[partida][i]:
+                for aux in self.edge[partida][i]:
+                    if day in aux and hchegada.transfer(aux.data_Inicio):
+                        resAux=self.dfs(i, destino, aux.data_Fim, day, rota)
+                        if resAux is not None:
+                            return aux.__str__()+"/"+resAux
+        return None
 
 def readFile():
     tempo = []
@@ -71,3 +128,7 @@ def findIndex(target, array):
         if array[i].partida == target:
             return i
     raise Exception('Valor não se encontrava no array!')
+
+def main():
+    connects = Arvore(readFile())
+    
