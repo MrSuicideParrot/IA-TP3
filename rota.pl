d107d731bcd793632aa1da78f416058519d5d416
André Cirne <ancirne@gmail.com>
2017-04-11 12:20:16 +0100

    O percurso já se encontra alinhavado

--- rota.pl
+++ rota.pl
@@ -31,53 +31,103 @@ route_r(Place1,PLace2,Day,Route,Visited):-
   append([PLace1-Aux:Flight_num:Dep_time:Arr_time],RouteAux,Route).
 
 /* ver esta função parece muito aldrabada */
-  approve([_]).
-  approve([]).
-  approve([_-_:_:Dep_time1:Arr_time1],[_-_:_:Dep_time2:Arr_time2|_]):-transfer(Arr_time1,Dep_time2).
+approve([_]).
+approve([]).
+approve([_-_:_:Dep_time1:Arr_time1],[_-_:_:Dep_time2:Arr_time2|_]):-transfer(Arr_time1,Dep_time2).
 
-  timetable(edinburgh,london,
-  [ 9:40/10:50/ba4733/alldays,
-  13:40/14:50/ba4773/alldays,
-  19:40/20:50/ba4833/[mo,tu,we,th,fr,su]]).
+/* Inicio do percurso */
+percurso(CidadeInicial, Pontos, Day1, Day2,Percurso):-
+  last(Pontos,Aux),
+  CidadeInicial = Aux,
+  false.
 
-  timetable(london,edinburgh,
-  [ 9:40/10:50/ba4732/alldays,
-  11:40/12:50/ba4752/alldays,
-  18:40/19:50/ba4822/[mo,tu,we,th,fr]]).
+percurso(CidadeInicial, Pontos, Day1, Day2,[CidadeInicial-Aux:Day1:Flight_num:Dep_time:Arr_time|PercRest]):-
+  %permutation(Pontos,PontosAuxiliares),
+  %member(Aux,PontosAuxiliares),
+  member(Aux,Pontos),
+  flight(CidadeInicial,Aux, Day1, Flight_num,Dep_time,Arr_time),
+  nextDay(Day1,Next),
+  delete(Aux,Pontos,PontosAuxiliares),
+  percurso_r(Aux, PontosAuxiliares, Target, Next, Day2, PercRest).
 
-  timetable(london,ljubljana,
-  [13:20/16:20/ju201/[fr],
-  13:20/16:20/ju213/[su]]).
+percurso_r(CidadeInicial, [],Target, Day1, Day2,[CidadeInicial-Target:Day1:Flight_num:Dep_time:Arr_time]):-
+  verifcaDia(Day1,Day2),
+  flight(CidadeInicial,Target, Day1, Flight_num,Dep_time,Arr_time).
 
-  timetable(london,zurich,
-  [ 9:10/11:45/ba614/alldays,
-  14:45/17:20/sr805/alldays]).
+percurso_r(CidadeInicial, Pontos, Target, Day1, Day2,Percurso):-
+  member(Aux,Pontos),
+  verifcaDia(Day1,Day2),
+  flight(CidadeInicial,Aux, Day1, Flight_num,Dep_time,Arr_time).
+  nextDay(Day1, Next),
+  delete(Aux,Pontos, PontosAuxiliares),
+  percurso_r(Aux, PontosAuxiliares, Target, Next, Day2, PercRest),
+  append([CidadeInicial-Aux:Day1:Flight_num:Dep_time:Arr_time],PercRest,Percurso).
 
-  timetable(london,milan,
-  [ 8:30/11:20/ba510/alldays,
-  11:00/13:50/az459/alldays]).
+percurso_r(CidadeInicial, Pontos, Target, Day1, Day2,Percurso):-
+  nextDay(Day1, Next),
+  verifcaDia(Next,Day2),
+  percurso_r(CidadeInicial, Pontos, Target, Next, Day2,Percurso).
 
-  timetable(ljubljana,zurich,
-  [11:30/12:40/ju322/[tu,th]]).
+/*verifcar dia  valido */
+verifcaDia(Day1,Day2):-
+  indexof(Index1,Day1,[mo,tu,we,th,fr,sa,su]),
+  indexof(Index2,Day2,[mo,tu,we,th,fr,sa,su]),
+  Index1 =< Index2.
 
-  timetable(ljubljana,london,
-  [11:10/12:20/yu200/[fr],
-  11:25/12:20/yu212/[su]]).
+/*Calculo do dia a seguir */
+nextDay(mo, tu).
+nextDay(tu, we).
+nextDay(we, th).
+nextDay(th, fr).
+nextDay(fr, sa).
+nextDay(sa, su).
+nextDay(su, mo).
 
-  timetable(milan,london,
-  [ 9:10/10:00/az458/alldays,
-  12:20/13:10/ba511/alldays]).
 
-  timetable(milan,zurich,
-  [ 9:25/10:15/sr621/alldays,
-  12:45/13:35/sr623/alldays]).
 
-  timetable(zurich,ljubljana,
-  [13:30/14:40/yu323/[tu,th]]).
+timetable(edinburgh,london,
+[ 9:40/10:50/ba4733/alldays,
+13:40/14:50/ba4773/alldays,
+19:40/20:50/ba4833/[mo,tu,we,th,fr,su]]).
 
-  timetable(zurich,london,
-  [ 9:00/9:40/ba613/[mo,tu,we,th,fr,sa],
-  16:10/16:55/sr806/[mo,tu,we,th,fr,su]]).
+timetable(london,edinburgh,
+[ 9:40/10:50/ba4732/alldays,
+11:40/12:50/ba4752/alldays,
+18:40/19:50/ba4822/[mo,tu,we,th,fr]]).
 
-  timetable(zurich,milan,
-  [ 7:55/8:45/sr620/alldays]).
+timetable(london,ljubljana,
+[13:20/16:20/ju201/[fr],
+13:20/16:20/ju213/[su]]).
+
+timetable(london,zurich,
+[ 9:10/11:45/ba614/alldays,
+14:45/17:20/sr805/alldays]).
+
+timetable(london,milan,
+[ 8:30/11:20/ba510/alldays,
+11:00/13:50/az459/alldays]).
+
+timetable(ljubljana,zurich,
+[11:30/12:40/ju322/[tu,th]]).
+
+timetable(ljubljana,london,
+[11:10/12:20/yu200/[fr],
+11:25/12:20/yu212/[su]]).
+
+timetable(milan,london,
+[ 9:10/10:00/az458/alldays,
+12:20/13:10/ba511/alldays]).
+
+timetable(milan,zurich,
+[ 9:25/10:15/sr621/alldays,
+12:45/13:35/sr623/alldays]).
+
+timetable(zurich,ljubljana,
+[13:30/14:40/yu323/[tu,th]]).
+
+timetable(zurich,london,
+[ 9:00/9:40/ba613/[mo,tu,we,th,fr,sa],
+16:10/16:55/sr806/[mo,tu,we,th,fr,su]]).
+
+timetable(zurich,milan,
+[ 7:55/8:45/sr620/alldays]).
